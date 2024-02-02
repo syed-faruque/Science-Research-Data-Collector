656b2d5d781c492d2019a1ac00a2a8469281507f
Syed Faruque <97230991+syed-faruque@users.noreply.github.com>
2024-02-01 22:53:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+//framework imports
 const express = require("express");
-const app = express();
 const parse = require('body-parser');
 const cors = require('cors');
 const session = require("express-session");
 const mysql = require("mysql2");
+
+
+
+
+// connection to the database
 const connection = mysql.createConnection({
 
     host: 'localhost',
@@ -13,6 +18,14 @@ const connection = mysql.createConnection({
 
 })
 
+
+
+//instance of express application
+const app = express();
+
+
+
+//middleware
 app.use(session({secret: 'key', resave: false, saveUninitialized: true}));
 app.use(cors());
 app.use(express.static(__dirname + "/frontend"));
@@ -21,7 +34,12 @@ app.use(express.json());
 
 
 
+
+
+
+//handles initial homepage choice
 app.post('/authentication', (req, res) => {
+    
     if (req.body.action == "signup") {
         res.sendFile(__dirname + "/frontend/signup.html");
     }
@@ -38,6 +56,9 @@ app.post('/authentication', (req, res) => {
 
 
 
+
+
+//checks the credentials entered during login phase
 app.post('/log', (req, res) => {
     username = req.body.username;
     password = req.body.password;
@@ -55,16 +76,16 @@ app.post('/log', (req, res) => {
 
 
 
+
+//inserts sign up credentials into database
 app.post('/newuser', (req, res) => {
     username = req.body.username;
     password = req.body.password;
-    insert = "Insert into credentials (username, password) values (?, ?)";
-    checker = "SELECT * FROM credentials WHERE username = ?";
-    connection.query(checker, [username], (err, results) => {
+    connection.query("SELECT * FROM credentials WHERE username = ?", [username], (err, results) => {
         if (results.length > 0) {
             res.sendFile(__dirname + "/frontend/exists.html");
         } else {
-            connection.query(insert, [username, password]);
+            connection.query("Insert into credentials (username, password) values (?, ?)", [username, password]);
             res.sendFile(__dirname + "/frontend/success.html");
         }
     });
@@ -73,6 +94,7 @@ app.post('/newuser', (req, res) => {
 
 
 
+// sends back username from session store for the client side
 app.get('/getusername', (req, res)=>{
     res.json({ username: req.session.username });
 });
@@ -80,19 +102,28 @@ app.get('/getusername', (req, res)=>{
 
 
 
+
+
+// appends logged data to the information database column
 app.post('/showprev', (req, res) => {
+
     const currentDate = new Date();
     const options = {month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true};
     const formattedDate = currentDate.toLocaleString('en-US', options);
     const info = req.body.information + ": added on " + formattedDate;
     sqlcommand = "UPDATE credentials SET information = CONCAT(COALESCE(information, ''), ?) WHERE username = ?";
-    connection.query(sqlcommand, [info + " * ", req.session.username], (err, results) => {
-        req.session.last_log = info;
+    connection.query(sqlcommand, [info + " * ", req.session.username], (err, results) => { //appends the new log and adds ' * ' at the end of it
+        req.session.last_log = info; //stores the last log in the user's session
         res.sendFile(__dirname + "/frontend/prevlog.html");
     });
 });
 
 
+
+
+
+
+// sends the previous log corresponding to the current user in the client side.
 app.get('/getprev', (req, res) =>{
 
     const information = req.session.last_log;
@@ -100,7 +131,14 @@ app.get('/getprev', (req, res) =>{
 
 })
 
+
+
+
+
+
+// authenticates admin
 app.post('/admin', (req, res) => {
+
     username = req.body.username;
     password = req.body.password;
     if ((username == "Syed Faruque") && (password == "batman")) {
@@ -111,11 +149,17 @@ app.post('/admin', (req, res) => {
 });
 
 
+
+
+
+
+
+
+// checks if the admin selected an existing user before sending the file to the previous logs
 app.post('/showlogs', (req, res) =>{
 
     user = req.body.user;
-    const checker = "SELECT * FROM credentials WHERE username = ?";
-    connection.query(checker, [user], (err, results)=>{
+    connection.query("SELECT * FROM credentials WHERE username = ?", [user], (err, results)=>{
         if (results.length > 0){
             req.session.desireduser = user;
             res.sendFile(__dirname + "/frontend/showdesiredlogs.html");
@@ -129,25 +173,37 @@ app.post('/showlogs', (req, res) =>{
 })
 
 
+
+
+
+
+
+//sends JSON containing usernames to the client-side so the admin can see it
 app.get('/getusers', (req, res) => {
 
     const getUsersQuery = "SELECT username FROM credentials";
     connection.query(getUsersQuery, (err, results) => {
         const usernames = results.map(result => result.username);
         res.json({ usernames: usernames });
+
     });
 
 })
 
 
+
+
+
+
+
+// creates a list containing the selected user's previous logs and sends it back to client-side
 app.get('/getlogs', (req, res) =>{
 
     const getInfoQuery = "SELECT information FROM credentials WHERE username = ?";
     connection.query(getInfoQuery, [req.session.desireduser], (err, results) => {
         const information = results[0].information;
-    
-        if (information && information.includes(' * ')) {
-            const infoList = information.split(' * ');
+        if (information && information.includes(' * ')) { //checks for ' * ' to see if a log exists 
+            const infoList = information.split(' * '); // creates the list of logs by splitting along ' * '
             res.json({ info: infoList });
         } else {
             console.log("no data");
@@ -158,4 +214,12 @@ app.get('/getlogs', (req, res) =>{
 
 
 
-app.listen(3000);
+
+
+
+// allows server to listen on port 3000
+app.listen(3000, () => {
+
+    console.log("server running on port 3000");
+    
+});
